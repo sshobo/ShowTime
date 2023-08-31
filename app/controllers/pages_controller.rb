c3dddb48daa1e133952b38b02ab2da18d43c71f5
Samuel Shobo <samuelshobo06@gmail.com>
2023-08-31 17:14:08 +0100

    homepage setup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,10 @@ class PagesController < ApplicationController
   def home
     sam = User.find_by(last_name: "Shobo")
     @feature_video = Video.find_by(user: sam)
+    @videos = Video.all
+    @top_tv = Video.where(category: "top_tv").sample(4)
+    @trending_movies = Video.where(category: "trending_movies").sample(4)
+    @top_movies = Video.where(category: "top_movies").sample(4)
   end
 
   def dashboard
