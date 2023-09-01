812a53a74dee1e9ed7af83290737e75b1f870859
Abhishek Mahendran <abhishekmy1301@gmail.com>
2023-09-01 15:41:09 +0100

    Joined genre tables

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,9 +5,10 @@ class PagesController < ApplicationController
     sam = User.find_by(last_name: "Shobo")
     @feature_video = Video.find_by(user: sam)
     @videos = Video.all
-    @top_tv = Video.joins(:videogenrejoins).where(videogenrejoins: { genre_id: 39} )
-    @trending_movies = Video.joins(:videogenrejoins).where(videogenrejoins: { genre_id: 35} )
-    @top_movies = Video.joins(:videogenrejoins).where(videogenrejoins: { genre_id: 33} )
+    jt = Video.joins(videogenrejoins: :genre)
+    @top_tv = jt.where(genres: {name: 'Thriller'})
+    @trending_movies = jt.where(genres: {name: 'Epic'})
+    @top_movies = jt.where(genres: {name: 'Epic'})
   end
 
   def dashboard
