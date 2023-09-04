3c0cb75563becc04abfc3c254aec14b2c69b684f
Abhishek <abhishekmy1301@gmail.com>
2023-09-04 14:11:34 +0100

    Revert "Searchsystem joan"

--- app/javascript/controllers/search_videos_controller.js
+++ app/javascript/controllers/search_videos_controller.js
@@ -2,30 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="search-videos"
 export default class extends Controller {
-  static targets = ["input", "videos", "users", "home"]
+  static targets = ["form", "input", "list"]
 
   update() {
-    const query = this.inputTarget.value;
-    const videos_url = `videos?query=${query}`
-    const users_url = `users?query=${query}`
-    if (query.trim() !== '') {
-      this.homeTarget.style.display = 'none';
-      fetch(videos_url, { headers: { "Accept": "text/plain" } })
-        .then(response => response.text())
-        .then((data) => {
-            this.videosTarget.innerHTML = data
-          })
-      fetch(users_url, { headers: { "Accept": "text/plain" } })
+    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
+    fetch(url, { headers: { "Accept": "text/plain" } })
       .then(response => response.text())
       .then((data) => {
-          this.usersTarget.innerHTML = data
-        })
-      this.homeTarget.style.display = 'none';
-    }
-    else{
-      this.videosTarget.innerHTML = '';
-      this.usersTarget.innerHTML = '';
-      this.homeTarget.style.display = '';
-    }
+        this.listTarget.outerHTML = data
+      })
   }
 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 
 require 'json'
 require 'open-uri'
-require 'faker'
 
 top_rated_movies_api_url = URI("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1")
 user_serialized = URI.open(top_rated_movies_api_url, 'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZWMzOWEwNTViN2IwNGYxM2RlZGRlYWEzYTMxNjA0YiIsInN1YiI6IjY0ZWUwNzc5ODM5MDE4MDExZjhlZjA2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.S6nNDVApwZoTYIzDMEhfRbdSH6otKXYIPH8H_8uM-dg').read
@@ -50,7 +49,7 @@ sam = User.create(
   creator: true
 )
 
-user_viewer = User.create(
+user_viewer = User.new(
   first_name: "Joan",
   last_name: "Alemany",
   email: "joanviewer@gmail.com",
@@ -58,42 +57,18 @@ user_viewer = User.create(
   creator: false
 )
 
-user_creator = User.create(
+user_creator = User.new(
   first_name: "Phillipe",
   last_name: "John-Jules",
   email: "phillipecreator@gmail.com",
   password: "123456",
   creator: true
 )
-10.times {
-  name = Faker::Name.name
-  name = name.split(" ")
-  first_name = name[0]
-  last_name = name[1]
-  email = "#{first_name}#{last_name}@gmail.com"
-  User.create(
-    first_name: first_name,
-    last_name: last_name,
-    email: email,
-    password: "123456",
-    creator: true
-  )
- }
 
- 10.times {
-  name = Faker::Name.name
-  name = name.split(" ")
-  first_name = name[0]
-  last_name = name[1]
-  email = "#{first_name}#{last_name}@gmail.com"
-  User.create(
-    first_name: first_name,
-    last_name: last_name,
-    email: email,
-    password: "123456",
-    creator: false
-  )
- }
+
+
+user_viewer.save
+user_creator.save
 
 user_creator.profile.attach(
   io: File.open('app/assets/images/menace2society.jpg'), # TO DO: update file
@@ -258,22 +233,6 @@ trending_tv_hash.each do |tv|
   create_video(tv, user_creator, "Trending TV", "tv")
 end
 
-puts "Creating reviews..."
-
-Review.create!(
-  rating: 4,
-  content: "One can only imagine the chilling impact on oceanic circulation patterns as temperature and pressure interplay during the ascent from the depths of the Atlantic abyss to the coastal waters.",
-  user: User.first,
-  video: Video.last
-)
-
-Review.create!(
-  rating: 2,
-  content: "Bradley Charles Cooper is an American actor and filmmaker. He is the recipient of various accolades, including a British Academy Film Award and two Grammy Awards",
-  user: User.second,
-  video: Video.last
-)
-
 puts "Displaying videos"
 
 Video.all.each do |video|
@@ -294,11 +253,16 @@ Videogenrejoin.all.each do |videogenrejoin|
   puts "--------------------------------"
 end
 
-puts "Displaying users"
+Review.create!(
+  rating: 4,
+  content: "One can only imagine the chilling impact on oceanic circulation patterns as temperature and pressure interplay during the ascent from the depths of the Atlantic abyss to the coastal waters.",
+  user: User.first,
+  video: Video.last
+)
 
-User.all.each do |user|
-  puts "First name: #{user.first_name}"
-  puts "Last name: #{user.last_name}"
-  puts "Gmail: #{user.email}"
-  puts "--------------------------------"
-end
+Review.create!(
+  rating: 2,
+  content: "Bradley Charles Cooper is an American actor and filmmaker. He is the recipient of various accolades, including a British Academy Film Award and two Grammy Awards",
+  user: User.second,
+  video: Video.last
+)
