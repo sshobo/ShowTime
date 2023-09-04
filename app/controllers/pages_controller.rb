f9508ac6de256c30c929d464634ba467049aa748
Phillipe John Jules <phillipejohnjules@gmail.com>
2023-09-04 17:31:04 +0100

    perfected dark mode and show page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
     sam = User.find_by(last_name: "Shobo")
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_09_04_132959) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,7 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_132959) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-
   create_table "casts", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "video_id", null: false
@@ -55,7 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_132959) do
     t.index ["user_id"], name: "index_casts_on_user_id"
     t.index ["video_id"], name: "index_casts_on_video_id"
   end
-
 
   create_table "crews", force: :cascade do |t|
     t.bigint "user_id", null: false
