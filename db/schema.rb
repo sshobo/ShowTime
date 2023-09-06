2deb2c80f8c817d96217704b7879b23f7f1bdc0d
Abhishek Mahendran <abhishekmy1301@gmail.com>
2023-09-06 14:43:11 +0100

    Views default value is 0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_06_112356) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_06_133619) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -115,7 +115,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_06_112356) do
 
   create_table "videos", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.integer "views"
+    t.integer "views", default: 0
     t.string "title"
     t.string "language"
     t.string "video_type"
